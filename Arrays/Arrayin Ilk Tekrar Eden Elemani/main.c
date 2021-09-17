ce363f48417f1792d26fe2830afab2d9d8a9ce13
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-09-18 00:17:41 +0300

    .

---
+++ Arrays/Arrayin Ilk Tekrar Eden Elemani/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int firstRepeating(int dizi[])
+{
+    int frekans[] = {0, 0, 0, 0, 0, 0, 0};
+    int i;
+
+    for (i = 0; i < 7; i++)
+    {
+
+        frekans[dizi[i] - 1]++;
+
+        if (frekans[dizi[i] - 1] > 1)
+        {
+
+            return dizi[i];
+        }
+    }
+
+    return 1;
+}
+int main()
+{
+    int dizi[] = {3, 5, 1, 5, 7, 7, 6};
+    int sayi = firstRepeating(dizi);
+    printf("First Repeating Element : %d", sayi);
+
+    return 0;
+}
