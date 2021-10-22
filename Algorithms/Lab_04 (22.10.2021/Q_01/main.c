d2dd62d016ae1e261518d5f8daf038d7d6a4b68f
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-10-22 09:39:32 +0300

    FSMVU Lab.

--- Algorithms/Lab_04 (22.10.2021/Q_01/main.c
+++ Algorithms/Lab_04 (22.10.2021/Q_01/main.c
@@ -25,10 +25,23 @@ void goster(void){
     srand(time(NULL));
     int array[10] = {0};
     int i;
+    int j;
+    int k;
 
-    for(i = 1 ; i <= 1000 ;i++)array[uret()]++;
-    
-    for(i = 1; i <= 9;i++) printf("%d Sayisi %d kere uretildi.\n",i,array[i]);
+    for(i = 1 ; i <= 100 ;i++)  array[uret()]++;
+    for(i = 1 ; i <= 9 ; i++) printf("%d Sayisi %d kere uretildi.\n",i,array[i]);
+
+    for(k = 1; k <= 9;k++){
+        printf("%d Sayisi = ",k);
+        for(j = 0 ; j < array[k] ;j++){
+            printf("*");
+        }
+        
+        printf("\n");
+
+      
+
+    }  
 
 }
 
