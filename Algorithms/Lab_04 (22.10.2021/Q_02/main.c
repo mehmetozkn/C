60b4332a18df7ac5cb5db3a5a9f8588d0336a002
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-10-22 10:14:18 +0300

    FSMVU Lab.

--- Algorithms/Lab_04 (22.10.2021/Q_02/main.c
+++ Algorithms/Lab_04 (22.10.2021/Q_02/main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void bul();
+void bul(char [], char[]);
 
 int main(int argc, const char *argv[])
 {
-    
-   bul();
-   
+    char kocaeli[8] = {'k','o','c','a','e','l','i','\0'};
+    char sakarya[8] = {'s','a','k','a','r','y','a','\0'};
+    bul(kocaeli, sakarya);
    
     printf("\n");
     return 0;
 }
 
-void bul(){
+void bul(char k[], char s[]){
 
-    char k[8] = {'k','o','c','a','e','l','i','\0'};
-    char s[8] = {'s','a','k','a','r','y','a','\0'};
     int i;
     int j;
     
-    for(i = 0 ; k[i] != '\0' ;i++){
+    for(i = 0 ; k[i] != '\0' ; i++){
         bool varMi = false;
         char temp = k[i];
         for(j = 0 ; s[j] != '\0';j++){
@@ -28,8 +26,6 @@ void bul(){
         }
          if(!varMi) printf("%c\n", temp);
     }
-
-
 
 }
 
