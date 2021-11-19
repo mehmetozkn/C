8179d3679850a41ee782e00c0f77faaf029ad021
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-11-19 11:51:11 +0300

    .

--- Algorithms/Characteristic Vector/Quiz_Week_07.java
+++ Algorithms/Characteristic Vector/Quiz_Week_07.java
@@ -1,76 +1,93 @@
 package com.company;
 
-public class Quiz_Week_07 {
-
+public class Main {
     public static void main(String[] args) {
 
-        char[] uni = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-        char[] a = {'c', 'e', 'f', 'h'};
-        char[] b = {'a', 'c', 'e', 'h'};
+        char[] U = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        char[] A = {'c', 'e', 'f', 'h'};
+        char[] B = {'a', 'c', 'e', 'h'};
 
-        int[] vectora = getCharacteristicVector(uni, a);
-        int[] vectorb = getCharacteristicVector(uni, b);
-        int[] vector1 = new int[uni.length];
+        int[] karA = getCharacteristicVector(U,A);
+        char[] intersection = intersection(U,A,B);
+        int[] kar_intersection = getCharacteristicVector(U,intersection);
 
-        for (int i = 0; i < uni.length; i++) {
-            if (vectora[i] != 0 && vectorb[i] != 0) {
-                vector1[i] = 1;
-            }
-        }
-
-        System.out.print("A  ∩  B = ");
-        print(uni, vector1);
-
-        System.out.println("");
-        int[] vector2 = getCharacteristicVector2(uni, b);
-        System.out.print("Complement of B = ");
-        print(uni, vector2);
-
-    }
-
-    public static int[] getCharacteristicVector(char[] uni, char[] arr) {
-
-        int sum[] = new int[uni.length];
-        for (int i = 0; i < uni.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (uni[i] == arr[j]) {
-                    sum[i] = 1;
-                    break;
-                } else {
-                    sum[i] = 0;
-                }
-            }
-        }
-        return sum;
-    }
-
-    public static int[] getCharacteristicVector2(char[] uni, char[] arr) {
-        int sum[] = new int[uni.length];
-        for (int i = 0; i < uni.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (uni[i] == arr[j]) {
-                    sum[i] = 0;
-                    break;
-                } else {
-                    sum[i] = 1;
-                }
-            }
-        }
-        return sum;
-    }
-
-    public static void print(char[] uni, int vector[]) {
-
-        for (int i = 0; i < vector.length; i++) {
-            System.out.print(vector[i]);
+        System.out.printf("A ∩ B = ");
+        for (int eleman:kar_intersection ) {
+            System.out.print(eleman);
         }
         System.out.print(" = ");
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i] == 1) {
-                System.out.print(uni[i] + " ");
+
+        for (char eleman: intersection) {
+            System.out.print(eleman+ " ");
+        }
+        System.out.println();
+        char[] compB = complement(U,B);
+        int[] karBComp = getCharacteristicVector(U,compB);
+
+        System.out.printf("U / B = ");
+        for (int eleman:karBComp ) {
+            System.out.print(eleman);
+        }
+        System.out.print(" = ");
+
+        for (char eleman: compB) {
+            System.out.print(eleman+ " ");
+        }
+
+    }
+
+    public static int[] getCharacteristicVector(char[] U, char[] arr) {
+        int[] karVektor = new int[U.length];
+        for (int i = 0; i < U.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(U[i] == arr[j]){
+                    karVektor[i] = 1;
+                    break;
+                }
+            }
+        }
+        return karVektor;
+    }
+
+    public static char [] intersection(char[] U, char[] B, char[] A){
+        int ortakSayisi = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if(A[i] == B[j]) ortakSayisi++;
+            }
+        }
+        char[] intersection = new char[ortakSayisi];
+        int k = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if(A[i] == B[j]) {
+                    intersection[k++] = A[i];
+                    break;
+                }
             }
         }
 
+        return intersection;
+    }
+
+    public static char [] complement(char[] U, char[] arr){
+        char[] complement = new char[U.length - arr.length];
+        int k = 0;
+        for (int i = 0; i < U.length; i++) {
+            boolean isExist = false;
+            for (int j = 0; j < arr.length; j++) {
+                if(U[i] == arr[j]){
+                    isExist = true;
+                    break;
+                }
+            }
+            if(!isExist){
+                complement[k++] = U[i];
+            }
+        }
+
+        return complement;
     }
 
 }
